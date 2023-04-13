2c937d7289dc3709e688169b13c31b76fed24f46
raul <jarquinr121@gmail.com>
2023-04-12 23:05:09 -0700

    UI partly finished

---
+++ src/routes/+page.svelte
@@ -0,0 +1,37 @@
+<div class="flex justify-center">
+	<div class="grid grid-cols-1 gap-4 min-w-full md:min-w-[750px]">
+		<h3 class= "text-center py-4">Create A Code Snippet</h3>
+		<div class="card p-5 w-full text-token space-y-4">
+			<label class="label">
+				<span>Snippet Title</span>
+				<input class="input" type="text" placeholder="Enter title here.."/>
+			</label>
+			<label class="label">
+				<span>Programming Language</span>
+				<select class="select">
+					<option value="HTML">HTML</option>
+					<option value="css">CSS</option>
+					<option value="Ts">TypeScript</option>
+				</select>
+			</label>
+			<label class="label">
+				<p>Code Snippet</p>
+			<textarea class="textarea" rows="4" placeholder="Enter your snippet code here.."></textarea>
+			</label>
+		</div>
+
+		<div class="text-center py-6">
+			<h3>My Code Snippets</h3>
+		</div>
+
+	</div>
+
+
+
+
+
+
+
+
+
+</div>
